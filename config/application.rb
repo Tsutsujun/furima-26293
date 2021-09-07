d9dfdc8a2fffd700867cbd0de37400e8ab38af39
Tsutsujun <kattou.surumono@gmail.com>
2021-09-08 06:10:17 +0900

    updates config/application.rb part.5

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Rails.application.config.assets.configure do |env|
-  env.export_concurrent = false
-end
-
 module Furima26293
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -27,4 +23,8 @@ module Furima26293
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
   end
+end
+
+Rails.application.config.assets.configure do |env|
+  env.export_concurrent = false
 end
