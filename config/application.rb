f5e5441364198714f89bae9b8124892b2434a27b
Tsutsujun <kattou.surumono@gmail.com>
2021-09-09 14:32:05 +0900

    undoes 'application.css.scss' to 'application.css' and updates config/application.rb
    一旦ファイル名を元に戻してから、アセットパイプラインを正常化してみる。

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Furima26293
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.assets.initialize_on_precompile = false
+
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
