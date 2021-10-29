b43882c9eaba7e083dfa1be42caf93e7cca0fa98
Tsutsujun <kattou.surumono@gmail.com>
2021-10-29 15:02:35 +0900

    adds validations to User-model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true, uniqueness: true
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
+  validates :given_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
+  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :given_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
 end
