6b9840df9dcc5483ffb6f854324572b8bd9d9b4c
Tsutsujun <kattou.surumono@gmail.com>
2021-10-29 15:18:01 +0900

    rewrites user.rb with ‘with_options’ option

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true, uniqueness: true
-  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
-  validates :given_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
-  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :given_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  with_options presence: true do
+    validates :nickname, uniqueness: true
+    validates :family_name, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
+    validates :given_name, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
+    validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :given_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+  end
 end
