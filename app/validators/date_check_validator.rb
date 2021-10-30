e13a929c3f8ec56473ee6e6b8e8039e520a52dfc
Tsutsujun <kattou.surumono@gmail.com>
2021-10-30 19:57:25 +0900

    writes the contents of validate_each-method

--- app/validators/date_check_validator.rb
+++ app/validators/date_check_validator.rb
@@ -1,5 +1,12 @@
 class DateCheckValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
 
+  def validate_each(record, attribute, value)
+    year, month, day = value.split('-')
+    if year.blank? || month.blank? || day.blank?
+      record.errors[attribute] << "Birth-date can't be blank."
+    elsif !(Date.valid_date?(year.to_i, month.to_i, day.to_i))
+      record.errors[attribute] << "Its birth-date doesn't exist."
+    end
   end
+
 end
