284314aa2e95ef3742d97bb017c3c6a4f5c7a397
Tsutsujun <kattou.surumono@gmail.com>
2021-11-18 19:40:46 +0900

    fixes date_check of the custom-validator part.3
    存在しない生年月日を入力して送信したときのエラーメッセージを少し変える。

--- app/validators/date_check_validator.rb
+++ app/validators/date_check_validator.rb
@@ -7,9 +7,9 @@ class DateCheckValidator < ActiveModel::EachValidator
     month = value[2]
     day   = value[3]
     if year.blank? || month.blank? || day.blank?
-      record.errors[attribute] << "Birth-date can't be blank."
+      record.errors[attribute] << "can't be blank."
     elsif !(Date.valid_date?(year.to_i, month.to_i, day.to_i))
-      record.errors[attribute] << "Its birth-date doesn't exist."
+      record.errors[attribute] << "can't be incorrect."
     end
   end
 
